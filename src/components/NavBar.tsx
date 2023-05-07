26c67191cacbc5d7ab6d5a6a6ded21738ecb1399
faizsarwar44 <faizsarwar44@gmail.com>
2023-05-08 03:11:13 +0500

    build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,16 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp"
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInputs from "./searchInputs";
 
 
 const NavBar = () => {
   return (
     <>
       {/* This helps in laying out components horizontally */}
-      <HStack justifyContent="space-between" padding="10px">
+      <HStack padding="10px">
         <Image src={logo} boxSize="70px"></Image>
+        <SearchInputs></SearchInputs>
         <ColorModeSwitch></ColorModeSwitch>
         {/* <Text>Navbar</Text> */}
       </HStack>
